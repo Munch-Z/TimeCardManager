3d6628fb9ef48b59db1f3f9abbaa234b692ef939
Nich Secord <nicholassecord@gmail.com>
2019-07-06 10:39:03 -0700

    more bp

---
+++ assets/js/timecard.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: 'AIzaSyBPNVKUzNYJnb1AhPUv5O9P0OvuTGGCIoc',
+    authDomain: 'in-class-06072019.firebaseapp.com',
+    databaseURL: 'https://in-class-06072019.firebaseio.com',
+    projectId: 'in-class-06072019',
+    storageBucket: '',
+    messagingSenderId: '1045843382384',
+    appId: '1:1045843382384:web:f34fb5614215dc26',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
